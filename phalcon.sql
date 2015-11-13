4adeec3fbb694a5dcec1caec8ab3b230a588bc36
takara <TakaraManabu@next-group.jp>
2015-11-13 16:57:25 +0900

    データベースの記述追加

---
+++ phalcon.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 172.17.42.1
+-- Generation Time: 2015 年 11 月 13 日 07:52
+-- サーバのバージョン： 5.5.44-0+deb8u1
+-- PHP Version: 5.4.45-0+deb7u2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phalcon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `book`
+--
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `title` varchar(64) DEFAULT NULL,
+  `author_name` varchar(32) DEFAULT NULL,
+  `detail` text,
+  `price` int(11) DEFAULT NULL,
+  `image` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
